a925590b578bd4c64fed1b833bd602eb72eaa6cb
Александр Чернилевский <aleksanderterar@gmail.com>
2021-01-22 02:11:06 +0300

    NEW

---
+++ script2.js
@@ -0,0 +1,39 @@
+window.setTimeout(function () {
+  let body = document.body;
+  let loader = document.getElementById("loader");
+  let string = window.location.search;
+  let url = 'https://api.github.com/users/AzezAeksandrChernilevskiy';
+  const date = new Date();
+  const getDate = new Promise((resolve, reject) => {
+    setTimeout(() => date ? resolve(date.innerHTML) : reject("Ошибка"), 100)
+  })
+  const getUrl = new Promise((resolve, reject) => {
+    setTimeout(() => url ? resolve(url) : reject("Ошибка URL!"), 100)
+  })
+
+  Promise.all([getUrl, getDate])
+    .then(([url, date]) => fetch(url))
+    .then(res => res.json())
+    .then(json => {
+      let avatar = new Image();
+      avatar.src = json.avatar_url;
+      body.append(avatar);
+      let name = document.createElement('p');
+      if(json.name != null) {
+        name.innerHTML = json.name;
+      } else {
+        name.innerHTML = 'Имя пользователя куда то пропало=(';
+      }
+      body.append(name);
+      name.addEventListener ("click", () => window.location = json.html_url);
+      let bio = document.createElement('p');
+      if(json.bio != null) {
+        bio.innerHTML = json.bio;
+      } else {
+        bio.innerHTML = 'Информация куда то пропала=(';
+      }
+      body.append(bio);
+      body.append(date);
+    })
+  .catch(error => console.log('Информация о пользователе куда то пропала=('))
+}, 3000);
